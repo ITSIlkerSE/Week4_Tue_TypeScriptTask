ecadefae37ede920eac3465f220abdc4763c23ef
ITSIlkerSE <itsilkerse@gmail.com>
2022-09-20 11:50:55 +0200

    Initial commit

---
+++ AddAndMultiply.ts
@@ -0,0 +1,30 @@
+function addOfVal1AndVal2(value1: number, value2: number) {
+
+    return value1 + value2;
+
+}
+
+function multiplyOfVal1AndVal2(value1: number, value2: number) {
+
+    return value1 * value2;
+}
+
+function subtractOfVal1AndVal3(value1: number, value2: number) {
+
+    return value1 - value2;
+}
+
+
+console.log(addOfVal1AndVal2(5, 12))
+console.log(multiplyOfVal1AndVal2(23, 55))
+console.log(subtractOfVal1AndVal3(55, 12))
+
+
+//    function subtractOfVal1AndVal3(value1:number, value2:number) {
+
+//    return value1 - value2;
+//    }
+//console.log(subtractOfVal1AndVal3("55", 12))
+//geht hier nicht, da 55 eine string ist und das Programm möchte eine number (ziffer/zahl wie in den parametern definiert)
+
+
